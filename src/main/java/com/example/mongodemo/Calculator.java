4d7c8962b660da28a357cdc6d168976b144803f4
anand47r@gmail.com <anand47r@gmail.com>
2020-12-16 00:13:50 -0500

    test9

---
+++ src/main/java/com/example/mongodemo/Calculator.java
@@ -0,0 +1,14 @@
+package com.example.mongodemo;
+
+public class Calculator {
+	
+	public int add(int x, int y) {
+		return x+y;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
